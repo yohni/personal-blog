bf276eb96586c98f052dedbf578a1467fe314b62
yohni <yohni.123@gmail.com>
2022-03-05 00:44:10 +0700

    feat: about page

---
+++ pages/about.js
@@ -0,0 +1,39 @@
+import { Container, Heading, Image, Link, Text } from "@chakra-ui/react";
+import React from "react";
+
+const About = () => {
+  return (
+    <Container
+      display="flex"
+      flexDir="column"
+      justifyContent="center"
+      minH={["2xl", "xl"]}
+      maxW={["sm", "container.md"]}
+      py={10}
+    >
+      <Heading as="h1" mb={10}>
+        Hello, I am Yohni
+      </Heading>
+      <Image
+        borderRadius={10}
+        boxSize={32}
+        mb={10}
+        src="/assets/Yohni.png"
+        alt="yohni"
+      />
+      <Text mb={4} color="gray.600">
+        Hi Friends! My name Yohni Finan. I came from my beloved country
+        Indonesia. I finished my study at 2020 and got a bachelor degree of
+        computer science in{" "}
+        <Link href="https://www.ugm.ac.id/en" isExternal>
+          Universitas Gadjah Mada
+        </Link>
+        . And now, I&apos;m working as Frontend on{" "}
+        <Link href="https://earnaha.com">Aha Learn-to-Earn</Link>.
+      </Text>
+      <Text></Text>
+    </Container>
+  );
+};
+
+export default About;
