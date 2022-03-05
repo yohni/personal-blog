6091733d12095ee9c1a567f0bdff478e9d989d5c
yohni <yohni.123@gmail.com>
2022-03-05 15:32:04 +0700

    feat: dot

---
+++ components/DotDivider/index.js
@@ -0,0 +1,12 @@
+import { Text } from "@chakra-ui/react";
+import React from "react";
+
+function Dot() {
+  return (
+    <Text color="gray.500" mx={2}>
+      &bull;
+    </Text>
+  );
+}
+
+export default Dot;
