9105b025e4e53ca409af8018c6c5a3e0771aaa2d
yohni <yohni.123@gmail.com>
2022-03-07 21:17:34 +0700

    feat: gtm setup

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,17 @@ import "@fontsource/be-vietnam-pro/400.css";
 import "@fontsource/ibm-plex-serif/700.css";
 import Head from "next/head";
 import { ChakraProvider } from "@chakra-ui/react";
+import TagManager from "react-gtm-module";
 import MainLayout from "../layouts/Main";
 import theme from "../theme";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({
+      gtmId: process.env.NEXT_PUBLIC_GTM_ID,
+    });
+  }, []);
   return (
     <>
       <Head>
